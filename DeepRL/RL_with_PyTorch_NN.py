a6a6c510445dd4bdc60d9d920d680d6ac8eb96bd
Manisha Senadeera <34785780+manishasena@users.noreply.github.com>
2020-05-03 13:06:21 +1000

    Update and rename DeepRL_helper.py to RL_with_PyTorch_NN.py

--- DeepRL/RL_with_PyTorch_NN.py
+++ DeepRL/RL_with_PyTorch_NN.py
@@ -1,9 +1,7 @@
 
 import numpy as np
 import random
-#import pandas as pd
 import matplotlib.pyplot as plt
-
 import torch
 
 WIN_STATE = 501 #1001
@@ -29,20 +27,18 @@ class AGENT:
                 elif param == "b":
                     self.weights[str(i)][param] = torch.randn(1, layers[i+1], device=device, dtype=dtype, requires_grad=True)
 
-
+        # Parameters for Adam optimiser
         self.step_size = alpha
         self.beta_m = beta_m
         self.beta_v = beta_v
         self.epsilon = epsilon
-
 
         # Initialize Adam algorithm's m and v
         self.m = [dict() for i in range(len(layers)-1)]
         self.v = [dict() for i in range(len(layers)-1)]
 
         for i in range(len(layers)-1):
-
-            # Initialize self.m[i]["W"], self.m[i]["b"], self.v[i]["W"], self.v[i]["b"] to zero
+            # Initialize Adam parameter values
             self.m[i]["W"] = np.zeros((layers[i], layers[i+1]))
             self.m[i]["b"] = np.zeros((1, layers[i+1]))
             self.v[i]["W"] = np.zeros((layers[i], layers[i+1]))
@@ -51,24 +47,24 @@ class AGENT:
         self.beta_m_product = self.beta_m
         self.beta_v_product = self.beta_v
 
-        self.mu = np.zeros([int(WIN_STATE-1-LOSE_STATE),1])
-
         for i in range(iterations):
 
             if i % 1 == 0:
                 print(i)
             
             self.CurrentState = START
-            
             self = AGENT.Cumulative_Reward(self, gamma,alpha)
 
-        #x = torch.tensor((np.linspace(-1,1,WIN_STATE).reshape(-1,1)), dtype=dtype)
+        # Final State Values
         self.Final_State = np.zeros([1,(WIN_STATE-1)])
         for i in range((WIN_STATE-1)):
             self.Final_State[0,i] = AGENT.Value_Prediction(self,i+1)
 
-    def one_hot_encoding(state):
 
+    def one_hot_encoding(state):
+        """
+        One hot coding if the input layer size is same as number of states
+        """
         coded_vector = np.zeros([1,(WIN_STATE-1)])
         coded_vector[0,state-1] = 1
 
@@ -89,8 +85,7 @@ class AGENT:
             
             if Next_state <= 0:
                 Next_state = LOSE_STATE
-
-            
+    
         elif act == "right":
             
             Next_state = self.CurrentState + hops
@@ -111,8 +106,7 @@ class AGENT:
             x = torch.tensor((x), dtype=dtype)
         
         v_hat = (x.mm(self.weights['0']['W'])+self.weights['0']['b']).clamp(min=0).mm(self.weights['1']['W']) + self.weights['1']['b']
-        #v_hat = x.mm(self.weights['0']).clamp(min=0).mm(self.weights['1'])
-
+        
         return v_hat
 
     def Reward(self):
@@ -126,46 +120,22 @@ class AGENT:
             
         return R
 
-    def mu_Update(self, state):
-   
-        self.mu[int(state-1)] += 1
-        
-        return self
-
     def SGD_Weight_Update(self,previous_state,R,alpha,gamma):
 
-        #x = torch.tensor(([[previous_state/1000-0.5]]), dtype=dtype)
-        #v_hat_S = (x.mm(self.weights['0'])+self.bias['0']).clamp(min=0).mm(self.weights['1']) + self.bias['1']
+        # Value estimate for previous state
         v_hat_S = AGENT.Value_Prediction(self,previous_state)
 
+        # Value estimate for current state
         if self.CurrentState == WIN_STATE:
             v_hat_S_dash = 0
         elif self.CurrentState == LOSE_STATE:
             v_hat_S_dash = 0
         else:
-            #x = torch.tensor(([[self.CurrentState/1000-0.5]]), dtype=torch.float)
-            #v_hat_S_dash = (x.mm(self.weights['0'])+self.bias['0']).clamp(min=0).mm(self.weights['1']) + self.bias['1']
             v_hat_S_dash = AGENT.Value_Prediction(self,self.CurrentState)
             v_hat_S_dash = float(v_hat_S_dash.detach().numpy())
 
         delta = R + gamma*v_hat_S_dash - float(v_hat_S.detach().numpy())
         v_hat_S.backward()
-
-        #print('Weight grad')
-        #print(self.weights['1'].grad)
-
-        #print('by hand weight')
-        #x = AGENT.one_hot_encoding(previous_state)
-        #x = torch.tensor((x), dtype=dtype)
-        #h = x.mm(self.weights['0'])
-        #h_relu = h.clamp(min=0)
-        #y_pred = h_relu.mm(self.weights['1'])
-
-        #grad_y_pred = 2.0 * (y_pred - (R + gamma*v_hat_S_dash))
-        #grad_w2 = h_relu.t().mm(grad_y_pred)
-        #print(grad_w2)
-        
-        mu = float(self.mu[int(previous_state-1)]/np.sum(self.mu))
         
         with torch.no_grad():
             self.weights['0']['W'] += alpha*delta*self.weights['0']['W'].grad
@@ -194,8 +164,6 @@ class AGENT:
         elif self.CurrentState == LOSE_STATE:
             v_hat_S_dash = 0
         else:
-            #x = torch.tensor(([[self.CurrentState/1000-0.5]]), dtype=torch.float)
-            #v_hat_S_dash = (x.mm(self.weights['0'])+self.bias['0']).clamp(min=0).mm(self.weights['1']) + self.bias['1']
             v_hat_S_dash = AGENT.Value_Prediction(self,self.CurrentState)
             v_hat_S_dash = float(v_hat_S_dash.detach().numpy())
 
@@ -207,10 +175,8 @@ class AGENT:
             for i in range(len(self.weights['0'])):
                 for param in self.weights[str(i)].keys():
 
-                    # g[i][param] = None
-                    ### START CODE HERE ###
                     g[i][param] = delta*self.weights[str(i)][param].grad
-                    #print(self.weights[str(i)][param].grad)
+
                     self.weights[str(i)][param].grad.zero_()
         
         for i in range(len(self.weights['0'])):
@@ -238,13 +204,10 @@ class AGENT:
         
         while (self.CurrentState != WIN_STATE) and (self.CurrentState != LOSE_STATE):
             
-                # Add state to list
                 previous_state = self.CurrentState
-
                 self = AGENT.mu_Update(self, previous_state)
                 
                 act, hops = AGENT.Policy(self,epsilon)
-
                 self = AGENT.Next_State(self,act,hops)
                 R = AGENT.Reward(self)
 
