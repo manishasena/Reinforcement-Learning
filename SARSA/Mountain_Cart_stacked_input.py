c3ff04de1a4cb678ee8c2f6e1e14c3d9362f5703
Manisha Senadeera <34785780+manishasena@users.noreply.github.com>
2020-05-06 20:10:48 +1000

    Update Mountain_Cart_stacked_input.py

--- SARSA/Mountain_Cart_stacked_input.py
+++ SARSA/Mountain_Cart_stacked_input.py
@@ -11,13 +11,9 @@ feature_range = [[env.min_position, env.max_position],[-1*env.max_speed,env.max_
 dtype = torch.float
 device = torch.device("cpu") 
 
-
 class Car:
 
-    def __init__(self, no_epsiodes,layers,alpha,gamma,beta_m,beta_v,epsilon,min_epsilon,feature_ranges, number_tilings, bins, offset):
-
-        self = Car.Car_initialise(self,layers)
-        #self = Car.Adam_Parameters(self,alpha,beta_m,beta_v,epsilon)
+    def __init__(self, no_epsiodes,alpha,gamma,epsilon,min_epsilon,feature_ranges, number_tilings, bins, offset):
 
         # Create tilings
         self.tilings = Car.create_tiling(feature_range, number_tilings, bins, offset)
@@ -26,20 +22,20 @@ class Car:
         self.lr = alpha
         self.gamma = gamma
 
-        self.state_sizes = [tuple(len(splits) + 1 for splits in tiling) for tiling in
-                            self.tilings]  # [(10, 10), (10, 10), (10, 10)]
+        self.state_sizes = [tuple(len(splits) + 1 for splits in tiling) for tiling in self.tilings]  
         self.q_tables = [np.zeros(shape=(state_size + (len(self.actions),))) for state_size in self.state_sizes]
-        print(self.q_tables)
-
 
         reduction = (epsilon - min_epsilon)/no_epsiodes
         
         for k in range(no_epsiodes):
 
+            print(k)
+
             epsilon -= reduction
 
-
-            if k%20 == 0:
+            # Print cost function every few episodes
+            # Visualise q-values of each state, using max action
+            if k%200 == 0:
                 print(k)
                 x_val = np.linspace(feature_range[0][0],feature_range[0][1],100)
                 y_val = np.linspace(feature_range[1][0],feature_range[1][1],100)
@@ -61,61 +57,10 @@ class Car:
 
                 plt.show()
 
-            print(k)
-
-            if k == (no_epsiodes - 10):
-                print('pause')
-
             # Initial action randomly selected
             act = random.choice([0,1,2])
             #Car.Action_verbalise(act)
             self = Car.Cumulative_Reward(self,act,alpha,gamma,epsilon,k)
-
-            #Visualise q_values:
-
-            
-    def create_tiling(feature_range, number_tilings, bins, offset):
-
-        """
-        feature_ranges: range of each feature; example: x: [-1, 1], y: [2, 5] -> [[-1, 1], [2, 5]]
-        number_tilings: number of tilings; example: 3 tilings
-        bins: bin size for each tiling and dimension; example: [[10, 10], [10, 10], [10, 10]]: 3 tilings * [x_bin, y_bin]
-        offsets: offset for each tiling and dimension; example: [[0, 0], [0.2, 1], [0.4, 1.5]]: 3 tilings * [x_offset, y_offset]
-        """
-
-        tilings = []
-
-        for i in range(number_tilings):
-
-            # For each tiling layer
-            tile = []
-            tile_bin = bins[i]
-            tile_offset = offset[i]
-
-            for j in range(len(feature_range)):
-                # The bins for each feature for that tile layer
-                feat_range = feature_range[j]
-
-                feat_tile = np.linspace(feat_range[0],feat_range[1],tile_bin[j]+1)[1:-1] + tile_offset[j]
-                tile.append(feat_tile)
-
-            tilings.append(tile)
-
-        tilings = np.array(tilings)
-
-        return tilings
-
-    def Car_initialise(self,layers):
-
-        self.weights = [dict() for i in range(len(layers)-1)]
-        for i in range(len(layers)-1):
-            for param in ["W","b"]:
-                if param == "W":
-                    self.weights[i][param] = torch.zeros(layers[i],layers[i+1], device=device, dtype = dtype, requires_grad=True)
-                elif param == "b":
-                    self.weights[i][param] = torch.zeros(1,layers[i+1], device=device, dtype = dtype, requires_grad=True)
-
-        return self
 
     def create_tiling(feature_range, number_tilings, bins, offset):
 
@@ -147,29 +92,6 @@ class Car:
         tilings = np.array(tilings)
 
         return tilings
-
-    def get_tiling(tilings,feature):
-
-        """
-        feature: sample feature with multiple dimensions that need to be encoded; example: [0.1, 2.5], [-0.3, 2.0]
-        tilings: tilings with a few layers
-        return: the encoding for the feature on each layer
-        """
-
-        feature_coding = []
-        for tile in tilings:
-
-            feature_tile = []
-
-            for i in range(len(feature)):
-                code = np.digitize(feature[i],tile[i])
-                feature_tile.append(code)
-
-            feature_coding.append(feature_tile)
-
-        feature_coding = np.array(feature_coding)
-
-        return feature_coding
 
     def Action_verbalise(action_no):
         
@@ -241,7 +163,7 @@ class Car:
     def update_weight(self, prev_position, prev_velocity, previous_action, reward):
         
         state = [prev_position, prev_velocity]
-        state_codings = Car.get_tile_coding(state, self.tilings)  # [[5, 1], [4, 0], [3, 0]] ...
+        state_codings = Car.get_tile_coding(state, self.tilings)  
         action_idx = previous_action
 
         for coding, q_table in zip(state_codings, self.q_tables):
@@ -261,35 +183,31 @@ class Car:
         self.CurrentVelocity = observation[1]
         self.CurrentAction = act
 
+        #t = 0
+        while not done: #(observation[0] < 0.5) and (t<600)
 
-        t = 0
-        while (observation[0] < 0.49) and (t < 1200): #not done: 
-            
-            t += 1
+            #t += 1
+
             previous_Position = self.CurrentPosition
             previous_Velocity = self.CurrentVelocity
             previous_action = self.CurrentAction
 
             action = Car.Policy(self,previous_Position,previous_Velocity,epsilon)
-            #Car.Action_verbalise(action)
 
             observation, reward, done, info = env.step(action)
-            if observation[0]>=0.49:
-                print(observation[0])
+
+            if observation[0] >= 0.5:
+                print('Reached!')
             
             # total reward
             total_reward += reward
 
             #env.render()
-            if k > 20:
-                env.render()
 
             self.CurrentPosition = observation[0]
             self.CurrentVelocity = observation[1]
             self.CurrentAction = action
 
-            #reward = 0
-            #print(reward)
             self = Car.update_weight(self, previous_Position, previous_Velocity, previous_action, reward)
 
         #print(total_reward)
@@ -302,11 +220,6 @@ epsilon = 0.1 #0.8 #0.0001
 min_epsilon = 0
 alpha = 0.5 #0.001
 gamma = 1
-no_trials = 1
-beta_m = 0.9
-beta_v = 0.999
-#layers = [(WIN_STATE-1), 100,  1]  #N, D_in, H, D_out
-layers = [3, 100,  1]
 
 #Tiling parameters
 number_tilings = 8
@@ -316,24 +229,5 @@ O1 = np.linspace(0,1,bin).reshape(-1,1)
 O2 = np.linspace(0,0.1,bin).reshape(-1,1)
 offset = np.concatenate([O1,O2],axis=1)   #[[0,0],[0.1,0.01],[0.2,0.02],[0.3,0.03],[0.4,0.04],[0.5,0.05],[0.6,0.06],[0.7,0.07]]
 
-ag = Car(no_epsiodes,layers,alpha,gamma,beta_m,beta_v,epsilon,min_epsilon,feature_range, number_tilings, bins, offset)
+ag = Car(no_epsiodes,alpha,gamma,epsilon,min_epsilon,feature_range, number_tilings, bins, offset)
 env.close()
-
-""" for _ in range(no_epsiodes):
-    done = False
-    env.reset()
-    while not done:
-        env.render()
-
-        # action = 0: left, action = 1: stationary, action = 2: right
-        action = env.action_space.sample()  #take a random action
-
-        observation, reward, done, info = env.step(action) 
-        print('Position: ' + str(observation[0]) + ' Velocity: ' + str(observation[1]))
-
-        # When to terminate epsiode:
-        if done:
-            print(done)
-            print('Episode finished')
-            #break
-env.close() """
