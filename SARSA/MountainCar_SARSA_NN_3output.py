648767289c267ebdc9b47f10186ebe11a7294a88
Manisha Senadeera <34785780+manishasena@users.noreply.github.com>
2020-05-25 10:36:47 +1000

    Update MountainCar_SARSA_NN_3output.py

--- SARSA/MountainCar_SARSA_NN_3output.py
+++ SARSA/MountainCar_SARSA_NN_3output.py
@@ -1,16 +1,21 @@
 import gym
 import torch
-import random
+#import random
 import numpy as np
+import torch.nn as nn
+import torch.optim as optim
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+from tqdm import tqdm, trange
 
 env = gym.make('MountainCar-v0')
+#env.seed(1); torch.manual_seed(1); np.random.seed(1)
 feature_range = [[env.min_position, env.max_position],[-1*env.max_speed,env.max_speed]]
 
 dtype = torch.float
 device = torch.device("cpu") 
 
+loss_fn = torch.nn.MSELoss()
 
 class Car:
 
@@ -21,27 +26,27 @@ class Car:
         # Initialise parameters for Adam optimiser
         self = Car.Adam_Parameters(self,alpha,beta_m,beta_v,epsilon)
 
+        # Layer information
         self.layers = layers
 
+        # Monitor performance
         self.TD_error = list()
         self.final_position = list()
+        self.success = 0 
 
+        # Parameters
         self.gamma = gamma
-
-        reduction = (epsilon - min_epsilon)/no_epsiodes
-        
-        for k in range(no_epsiodes):
-
-            #print(k)
-
-            # Reduce exploration in each step
-            epsilon -= reduction
+        self.alpha = alpha
+        self.epsilon = epsilon
+ 
+   
+        for k in trange(no_epsiodes):
 
             # Print cost function every few episodes
             # Visualise q-values of each state, using max action
-            if k%1000 == 0:
+            if k%(no_epsiodes-1) == 0:
                 plt.plot(self.TD_error)
-                plt.ylim(-5,5)
+                plt.ylim(0,15)
                 plt.show()
 
                 plt.plot(self.final_position)
@@ -59,7 +64,9 @@ class Car:
 
                         X[r,l] = x_val[l]
                         Y[r,l] = y_val[r]
-                        Z[r,l] = -1*max([Car.Action_Value(self,x_val[l],y_val[r],0),Car.Action_Value(self,x_val[l],y_val[r],1),Car.Action_Value(self,x_val[l],y_val[r],2)])
+                        val, _ = torch.max(Car.Action_Value(self,x_val[l],y_val[r]),-1)
+                        Z[r,l] = -1*val
+                        #Z[r,l] = -1*max([Car.Action_Value(self,x_val[l],y_val[r],0)[0],Car.Action_Value(self,x_val[l],y_val[r],1)[1],Car.Action_Value(self,x_val[l],y_val[r],2)[2]])
 
                 fig = plt.figure()
                 ax = fig.add_subplot(111, projection='3d')
@@ -68,26 +75,26 @@ class Car:
 
                 plt.show()
 
-            # Initial action randomly selected
-            act = random.choice([0,1,2])
-            #Car.Action_verbalise(act)
-            self = Car.Cumulative_Reward(self,act,alpha,gamma,epsilon,k)
+                print(self.success)
+
+            self = Car.Cumulative_Reward(self,gamma,k)
             
 
     def Car_initialise(self,layers):
 
         self.weights = [dict() for i in range(len(layers)-1)]
-
+        
         for i in range(len(layers)-1):
             self.weights[i] = {}
-            for param in ["W","b"]:
+            for param in ["W","b"]: 
                 if param == "W":
-                    #self.weights[act][i][param] = torch.zeros(layers[i],layers[i+1], device=device, dtype = dtype, requires_grad=True)
                     self.weights[i][param] = torch.randn(layers[i],layers[i+1], device=device, dtype = dtype, requires_grad=True)
+                    #self.weights[i][param] = torch.nn.Linear(layers[i],layers[i+1], bias=False)
                 elif param == "b":
-                    #self.weights[i][param] = torch.zeros(1,layers[i+1], device=device, dtype = dtype, requires_grad=True)
                     self.weights[i][param] = torch.randn(1,layers[i+1], device=device, dtype = dtype, requires_grad=True)
+                    #self.weights[i][param] = torch.nn.Linear(1,layers[i+1], bias=False)
 
+        
         return self
 
     def Adam_Parameters(self,alpha,beta_m,beta_v,epsilon):
@@ -129,75 +136,81 @@ class Car:
         #Select which next action to take. 
         #This is defined as the action with the highest state value
 
-        if np.random.uniform() <= epsilon:
-            action = random.choice([0,1,2])
+        if epsilon == 0:
+            _, action = torch.max(Car.Action_Value(self,position,velocity),-1)
+            action = action.item()
+
         else:
-
-            q0 = Car.Action_Value(self,position,velocity,0)
-            q1 = Car.Action_Value(self,position,velocity,1)
-            q2 = Car.Action_Value(self,position,velocity,2)
-
-            action = np.argmax([q0,q1,q2])
+            if np.random.rand(1) < epsilon:
+                action = np.random.randint(0,3)
+            else:
+                _, action = torch.max(Car.Action_Value(self,position,velocity),-1)
+                action = action.item()
 
         return action
 
-    def Action_Value(self,position,velocity,act):
+    def Action_Value(self,position,velocity):
 
-        # Normalise inputs to NN
-        position = (position + 1.2)/(0.5+1.2)
-        velocity = (velocity +0.07)/(0.07 + 0.07)
-        #act = act/2
-
-        #x = torch.tensor([[position,velocity,act]], dtype = dtype)
         x = torch.tensor([[position,velocity]], dtype = dtype)
+        #x = x.reshape(-1,1)
+
+        """
+        model = torch.nn.Sequential(
+            self.weights[0]["W"],
+            self.weights[1]["W"]
+        )
+        """
 
         for i in range(len(self.layers)-1):
-            x = (x.mm(self.weights[i]["W"])+self.weights[i]['b'])
-            #x = x.mm(self.weights[i]["W"])
-            if i != (len(self.layers)-2):
-                x = x.clamp(min=0) #ReLU
-                x = torch.sign(x)
-            #if i % 2 == 0:
-            #    x = x.clamp(max=0) #ReLU
-            #else:
-            #    x = torch.sigmoid(x)
-            
-        #x = x.clamp(max=0) #ReLU
+
+            #x = torch.mm(self.weights[i]["W"].weight,x) # + self.weights[i]['b'].weight
+            x = torch.mm(x,self.weights[i]["W"])
+            #x = torch.mm(x,self.weights[i]["W"]) + self.weights[i]['b']
+
+            #if i != (len(self.layers)-2):
+            #    x = x.clamp(min=0) #ReLU
+            #    x = torch.sign(x)
         
-        q_val = x[0][act]
+        #q_val = model(x)
+        q_val = x.reshape(1,-1)
 
         return q_val
 
-    def SGD_Weight_Update(self,previous_position,previous_velocity,prev_action,done,R,alpha):
+    def SGD_Weight_Update(self,next_Position,next_Velocity,next_action,done,reward,alpha):
 
-        # Value estimate for previous state
-        #v_hat_S = AGENT.Value_Prediction(self,previous_state)
-        q_hat_S_a = Car.Action_Value(self,previous_position,previous_velocity,prev_action)
+        ############### Update weights
+
+        q_hat_S_a = Car.Action_Value(self,self.CurrentPosition,self.CurrentVelocity)
+        q_hat_S_a = q_hat_S_a[0]
 
         # Value estimate for current state
-        if self.CurrentPosition >= 0.5:
-            q_hat_S_a_dash = 0
-        else:
-            #v_hat_S_dash = AGENT.Value_Prediction(self,self.CurrentState)
-            #v_hat_S_dash = float(v_hat_S_dash.detach().numpy())
-            q_hat_S_a_dash = Car.Action_Value(self,self.CurrentPosition,self.CurrentVelocity,self.CurrentAction)
-            q_hat_S_a_dash = float(q_hat_S_a_dash.detach().numpy())
+        q_hat_S_a_dash = Car.Action_Value(self,next_Position,next_Velocity)
+        q_hat_S_a_dash = q_hat_S_a_dash[0]
 
-        q_hat_S_a.backward()
-        delta = R + self.gamma*q_hat_S_a_dash - float(q_hat_S_a.detach().numpy())
-        #delta =-1*(R + self.gamma*q_hat_S_a_dash - q_hat_S_a)
-        self.TD_error.append(delta)
-        #delta.backward()
-        
-        
+        Q_target = q_hat_S_a.clone()
+        Q_target = Q_target.data
+        Q_target[self.CurrentAction] = reward + torch.mul(gamma,q_hat_S_a_dash[next_action].detach())
+        Q_target = Q_target.reshape(q_hat_S_a.shape) 
+    
+        loss = loss_fn(q_hat_S_a,Q_target)
+        loss.backward()
+
+        self.TD_error.append(loss.item())
+    
         with torch.no_grad():
             for i in range(len(self.layers)-1):
-                self.weights[i]['W'] += alpha*delta*self.weights[i]['W'].grad
-                self.weights[i]['b'] += alpha*delta*self.weights[i]['b'].grad
+                #self.weights[i]['W'].weight -= self.alpha*self.weights[i]['W'].weight.grad
+                self.weights[i]['W'] -= self.alpha*self.weights[i]['W'].grad
+                
+                #self.weights[i]['b'].weight -= self.alpha*self.weights[i]['b'].weight.grad
+                #self.weights[i]['b'] -= self.alpha*self.weights[i]['b'].grad
 
                 # Manually zero the gradients after updating weights
+                #self.weights[i]['W'].weight.grad.zero_()
                 self.weights[i]['W'].grad.zero_()
-                self.weights[i]['b'].grad.zero_()
+                
+                #self.weights[i]['b'].weight.grad.zero_()
+                #self.weights[i]['b'].grad.zero_()
 
         return self
 
@@ -215,9 +228,10 @@ class Car:
             q_hat_S_a_dash = Car.Action_Value(self,self.CurrentPosition,self.CurrentVelocity,self.CurrentAction)
             q_hat_S_a_dash = float(q_hat_S_a_dash.detach().numpy())
 
+        q_hat_S_a.backward()
         delta = reward + self.gamma*q_hat_S_a_dash - float(q_hat_S_a.detach().numpy())
         self.TD_error.append(delta)
-        q_hat_S_a.backward()
+        
 
         # Get gradient of each weight
         g = [dict() for i in range(len(self.weights[0]))]
@@ -250,59 +264,83 @@ class Car:
 
         return self
 
-    def Cumulative_Reward(self,act,alpha,gamma,epsilon,k):
-        env.reset()
-        observation, reward, done, info = env.step(act) # take a random action
+    def Cumulative_Reward(self,gamma,k):
+        
+        observation = env.reset()
+
+        # Initial action randomly selected
+        #act = random.choice([0,1,2])
+        #observation, reward, done, info = env.step(act) # take a random action
 
         # total reward
-        total_reward = reward
+        total_reward = 0
 
-        self.CurrentPosition = observation[0]
-        self.CurrentVelocity = observation[1]
-        self.CurrentAction = act
+        max_position = -0.7
 
-        t = 0
-        while (observation[0] < 0.5) and (t<600): #not done: 
-            
-            t += 1
-            
-            previous_Position = self.CurrentPosition
-            previous_Velocity = self.CurrentVelocity
-            previous_action = self.CurrentAction
+        for s in range(2000): #not done: 
 
-            action = Car.Policy(self,previous_Position,previous_Velocity,epsilon)
+            self.CurrentPosition = observation[0]
+            self.CurrentVelocity = observation[1]
 
+            # Given S, select A
+            action = Car.Policy(self,self.CurrentPosition,self.CurrentVelocity,self.epsilon)
+            self.CurrentAction = action
+
+            #print(observation)
+            # Take action, and return reward and S'
             observation, reward, done, info = env.step(action)
 
+            if k > 2800:
+                env.render()
+
+            if observation[0] > max_position:
+                max_position = observation[0]
+
+            next_Position = observation[0]
+            next_Velocity = observation[1]
+            
+
+            ### SARSA Option ###
+            # Given S', select next A
+            action = Car.Policy(self,next_Position,next_Velocity,self.epsilon)
+            next_action = action
+            
+            #observation, reward, done, info = env.step(action)
+
             if observation[0] >= 0.5:
+                #print(k)
                 print('Reached!')
+                self.success += 1
+                print(self.success)
+                self.alpha = self.alpha*0.9
+                self.epsilon = self.epsilon*0.99
             
             # total reward
             total_reward += reward
 
-            #env.render()
-
-            self.CurrentPosition = observation[0]
-            self.CurrentVelocity = observation[1]
-            self.CurrentAction = action
+            if done:
+                break
+            ########################
 
             #self = Car.Adam_Weight_Update(self,previous_Position,previous_Velocity,previous_action,done,reward,alpha,epsilon)
-            self = Car.SGD_Weight_Update(self,previous_Position,previous_Velocity,previous_action,done,reward,alpha)
+            self = Car.SGD_Weight_Update(self,next_Position,next_Velocity,next_action,done,reward,alpha)
 
-        self.final_position.append(observation[0])
+        self.final_position.append(max_position)
 
         return self
 
 
-no_epsiodes = 5000
+no_epsiodes = 3000
 epsilon = 0.3 #0.8 #0.0001
-min_epsilon = 0
+min_epsilon = 0.25
 alpha = 0.001 #0.001
-gamma = 0.90
+gamma = 0.99
+observation = env.reset()
+success = 0
 
 no_trials = 1
-beta_m = 0.8
-beta_v = 0.9
+beta_m = 0.9
+beta_v = 0.999
 #layers = [(WIN_STATE-1), 100,  1]  #N, D_in, H, D_out
 #layers = [3, 1000,  1]
 layers = [2, 200, 3]
